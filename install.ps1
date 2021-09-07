776bfcf28855cf1f9105ea372c7c95497a902329
roaldi <65696348+roaldi@users.noreply.github.com>
2021-09-07 06:37:53 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,8 +5,13 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
+
 #Use choco to install python
 choco install python
+choco install git
+
+refreshenv
 
 #Install all required python packages
 pip install flask
